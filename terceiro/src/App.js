5fad1c828167508ab4131e32739b89d427532cc1
LEONARDO ISAIAS DA SILVA COSTA <leonardo.iscosta@senacsp.edu.br>
2022-05-13 10:58:07 -0300

    Contador

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,17 +1,12 @@
+import { useState } from "react";
+
 function App() {
-  let valor1 = Math.round(Math.random() * 200);
-  let valor2 = Math.round(Math.random() * 200);
+  const [n, setN] = useState(1);
 
   return (
     <div>
-      <p> A soma entre 50 e 10 é {50 + 10}</p>
-      <p>
-        A soma entre {valor1} e {valor2} é {valor1 + valor2}{" "}
-      </p>
-      <p>
-        {" "}
-        O resultado acima é {(valor1 + valor2) % 2 === 0 ? "Par" : "Ímpar"}
-      </p>
+      <p> {n} </p>
+      <button onClick={() => setN(n + 1)}>Clique</button>
     </div>
   );
 }
