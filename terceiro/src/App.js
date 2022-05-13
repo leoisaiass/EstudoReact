ca71527e7657ac57eba98de8fef7abf9d53a0323
LEONARDO ISAIAS DA SILVA COSTA <leonardo.iscosta@senacsp.edu.br>
2022-05-13 11:54:07 -0300

    Criação de produtos

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,12 +1,59 @@
 import { useState } from "react";
 
 function App() {
-  const [n, setN] = useState(1);
+  const [produto, setProduto] = useState([
+    {
+      id: 500,
+      nome: "Mouse",
+      categoria: "Informática",
+      quantidade: 15,
+      preco: 50.0,
+    },
+
+    {
+      id: 501,
+      nome: "Teclado",
+      categoria: "Informática",
+      quantidade: 20,
+      preco: 150.9,
+    },
+  ]);
 
   return (
     <div>
-      <p> {n} </p>
-      <button onClick={() => setN(n + 1)}>Clique</button>
+      <button
+        onClick={() => {
+          var cod = prompt("Entre com o código");
+          var nom = prompt("Entre com o nome do produto");
+          var cat = prompt("Entre com a categoria");
+          var qtd = prompt("Entre com a quantidade");
+          var pre = prompt("Entre com o preço");
+
+          setProduto(
+            produto.concat({
+              id: cod,
+              nome: nom,
+              categoria: cat,
+              quantidade: qtd,
+              preco: pre,
+            })
+          );
+        }}
+      >
+        Clique
+      </button>
+
+      {produto.map((item, index) => (
+        <div key={index}>
+          <h2>{item.nome}</h2>
+          <ul>
+            <li>Código: {item.id}</li>
+            <li>Produto: {item.categoria}</li>
+            <li>Quantidade: {item.quantidade}</li>
+            <li>Preço: {item.preco}</li>
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
