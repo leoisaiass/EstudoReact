ad6cf2d397b63c696ab3c6381b681744277a0ed4
LEONARDO ISAIAS DA SILVA COSTA <leonardo.iscosta@senacsp.edu.br>
2022-05-13 09:18:21 -0300

    Execução de JS dentro da área de retorno

---
+++ terceiro/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  let valor1 = Math.round(Math.random() * 200);
+  let valor2 = Math.round(Math.random() * 200);
+
+  return (
+    <div>
+      <p> A soma entre 50 e 10 é {50 + 10}</p>
+      <p>
+        A soma entre {valor1} e {valor2} é {valor1 + valor2}{" "}
+      </p>
+      <p>
+        {" "}
+        O resultado acima é {(valor1 + valor2) % 2 === 0 ? "Par" : "Ímpar"}
+      </p>
+    </div>
+  );
+}
+
+export default App;
